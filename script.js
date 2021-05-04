afde2b4900b26b249b78754a6da9c31412a753df
Emily Gertsch <gertsch21224@lcsd2kids.org>
2021-05-04 10:42:53 -0600

    working on the grid in js file

---
+++ script.js
@@ -0,0 +1,12 @@
+const gridContainer = document.querySelector ("#grid-container");
+let numOfTimes = 256;
+for( let i = 0; i<numOfTimes; i++) {
+const singleCell = document.createElement ("div");
+singleCell.textContent = "hello";
+singleCell.style.color = "blue";
+singleCell.classList.add ("bg-black")
+
+
+
+
+}
