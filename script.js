d2341f3887886e1218b14d698a15b119d4a408d3
Emily Gertsch <gertsch21224@lcsd2kids.org>
2021-05-04 11:09:15 -0600

    Edited some stuff

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 const gridContainer = document.querySelector ("#grid-container");
-let numOfTimes = 256;
-for( let i = 0; i<numOfTimes; i++) {
-const singleCell = document.createElement ("div");
-singleCell.textContent = "hello";
-singleCell.style.color = "blue";
-singleCell.classList.add ("bg-black")
 
-
-
-
+let rows = document.getElementsByClassName ("gridRow");
+let cells = document.getElementsByClassName("cell");
+function defaultGrid() {
+    makeRows(16);
+    makeColumns(16);
 }
+// let numOfTimes = 16;
+// for( let numOfTimes= 0; i<numOfTimes; i++) {
+// const singleCell = document.createElement ("div");
+// singleCell.textContent = "hello";
+// singleCell.style.color = "grey";
+// singleCell.classList.add ("bg-black")
+
+
+singleCell.addEventListener ("click" , () => {
+    alert ("hello world");
+});
+gridContainer.appendChild (singleCell);
+
