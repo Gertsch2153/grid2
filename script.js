6167e6105508703c5ce52ea310aec41f47551ad8
Emily Gertsch <gertsch21224@lcsd2kids.org>
2021-05-06 11:15:33 -0600

    Changed the size of grid and changed the color to grey

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 const gridContainer = document.querySelector ("#grid-container");
+const root = document.documentElement;
 let userChoice = 16
+root.style.setProperty('--width', (userChoice)+"px")
 for(let j = 0; j<userChoice;j++){
     const rowOfCells = document.createElement ("div");
     rowOfCells.classList.add("row");
     for( let i = 0; i<userChoice; i++) {
         const singleCell = document.createElement ("div");
-         singleCell.textContent = "hello";
-         singleCell.style.color = "gray";
-         singleCell.classList.add ("bg-black") 
-         singleCell.addEventListener ("mouseover" , () => {
-            singleCell.style.color = "purple";
-           
-         });
-         rowOfCells.appendChild (singleCell);
-
-        
+        singleCell.innerHTML = "&nbsp;";
+        singleCell.style.color = "gray";
+        singleCell.classList.add ("cell") 
+        singleCell.addEventListener ("mouseover" , () => {
+            singleCell.style.backgroundColor = "purple";
+        });
+        rowOfCells.appendChild (singleCell);
     }
     gridContainer.appendChild(rowOfCells)
 }
+
 
 
 
